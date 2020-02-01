dcf0ded8cce81ebff06e541283e3d3c1652427dc
kevincbryan <kevincbryan@gmail.com>
2020-02-01 11:30:48 -0800

    Completed public function for Material Manip

--- Assets/Shader/MaterialTest.cs
+++ Assets/Shader/MaterialTest.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class MaterialTest : MonoBehaviour
 {
     Material m_Material;
+    ShaderManip m_ShaderManip;
     // Start is called before the first frame update
     void Start()
     {
-        m_Material = GetComponent<Renderer>().material;
-        Debug.Log(m_Material);
-        Debug.Log(m_Material.GetFloat("OneForFire"));
+        m_ShaderManip = GetComponent<ShaderManip>();
+        m_ShaderManip.Fire(.5f);
+        m_ShaderManip.Air(.5f);
+        //m_Material = GetComponent<Renderer>().material;
+       // Debug.Log(m_Material);
+      //  Debug.Log(m_Material.GetFloat("OneForFire"));
 
-        m_Material.SetFloat("OneForFire", 1.0f);
-        Debug.Log(m_Material.GetFloat("OneForFire"));
+       // m_Material.SetFloat("OneForFire", 1.0f);
+       // Debug.Log(m_Material.GetFloat("OneForFire"));
     }
 
     // Update is called once per frame
--- Assets/Shader/ShaderManip.cs
+++ Assets/Shader/ShaderManip.cs
@@ -8,7 +8,7 @@ public class ShaderManip : MonoBehaviour
     private float fireMin = 0f;
     private float fireMax = 1f;
     private float airMin = 0f;
-    private float airMax = 0f;
+    private float airMax = 1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,15 +16,22 @@ public class ShaderManip : MonoBehaviour
 
     }
 
-    void Fire (float input)
+
+    //Changes the OnFire Value to a value between 0 and 1 given to the function
+    //A higher OnFire makes it more burny, a lower one makes it more normal.
+    public void Fire (float input)
     {
         if (input < fireMin) input = fireMin;
         if (input > fireMax) input = fireMax;
         m_Material.SetFloat("OneForFire", input);
 
     }
-    void air(float input)
+
+    //Changes the Air Value to a value between 0 and 1 given to the function
+    //A higher Air makes it more airless, a lower one makes it more normal.
+    public void Air(float input)
     {
+        //Debug.Log("Air has been fired");
         if (input < airMin) input = airMin;
         if (input > airMax) input = airMax;
         m_Material.SetFloat("OneForAirless", input);
