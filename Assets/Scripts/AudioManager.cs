60f7e60a677be0777d5f3e1c49405edeb88d8a11
pacificyost <pacificyost@gmail.com>
2020-02-01 15:52:39 -0800

    Audio Manager, Game Initializer, SceneLoader helper

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+    
+    public AudioSource BGM;
+    private AudioSource BGMInstance;
+
+	// Use this for initialization
+	void Start () {
+        if (FindObjectsOfType<AudioManager>().Length > 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+
+        BGMInstance = GameObject.Instantiate(BGM, this.transform);
+        StartMusic();
+	}
+
+    public void StartMusic()
+    {
+        BGMInstance.Play();
+    }
+	
+	
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public static class SceneLoader  {
+    public static void GoToScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+
+    }
+
+    public static void GoToNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
