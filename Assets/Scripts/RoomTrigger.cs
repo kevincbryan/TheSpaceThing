d9d3cab29d0c16d46ddf1818033c81ae2e34ac44
David S <git@davidsharer.com>
2020-02-01 15:55:48 -0800

    Health and Rooms

---
+++ Assets/Scripts/RoomTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTrigger : MonoBehaviour
+{
+    public Room room;
+
+    public void OnTriggerEnter(Collider collider) {
+        if (collider.tag == "Player") {
+            collider.gameObject.GetComponent<PlayerInteraction>().OnRoomEnter(this);
+        }
+    }
+
+    public void OnTriggerExit(Collider collider) {
+        if (collider.tag == "Player") {
+            collider.gameObject.GetComponent<PlayerInteraction>().OnRoomExit(this);
+        }
+    }
+}
