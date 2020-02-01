f306ddb45f7e009c8ee935f430e4a99b4bda791d
David S <git@davidsharer.com>
2020-02-01 12:12:16 -0800

    yes

---
+++ Assets/Scripts/MeshGizmo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshGizmo : MonoBehaviour
+{
+    public Color color = new Color(1, 0, 0, 0.5f);
+
+    private Mesh mesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = gameObject.GetComponent<MeshFilter>().sharedMesh;
+        
+        mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
+        mesh.RecalculateTangents();
+    }
+    
+    void OnDrawGizmos() {
+        Gizmos.color = color;
+        Gizmos.DrawMesh(
+            gameObject.GetComponent<MeshFilter>().mesh,
+            0,
+            gameObject.transform.position,
+            gameObject.transform.rotation,
+            gameObject.transform.localScale
+        );
+    }
+}
