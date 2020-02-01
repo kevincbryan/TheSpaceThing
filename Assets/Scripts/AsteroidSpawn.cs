33a9a9e45183c428dac853f40240ff1a3883ebff
pacificyost <pacificyost@gmail.com>
2020-02-01 11:28:51 -0800

    Initial Asteroid spawner

---
+++ Assets/Scripts/AsteroidSpawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpawn : MonoBehaviour
+{
+    [SerializeField] GameObject asteroidPrefab;
+
+    [SerializeField] float spawnRate = 1f;
+    [SerializeField] float startDelay = 15f;
+
+    [SerializeField] float spawnDistance = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", startDelay, spawnRate);
+    }
+
+    void Spawn()
+    {
+        Vector3 spawnLocation = new Vector3(Random.Range(-1f,1f), 0f, Random.Range(-1f,1f)).normalized * spawnDistance;
+        spawnLocation = new Vector3(spawnLocation.x, 1f, spawnLocation.z);
+
+        GameObject newAsteriod = Instantiate(asteroidPrefab, spawnLocation, Quaternion.identity, transform);
+    }
+}
