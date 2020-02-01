9efbd1a40dcda7838f3325e041a1c7d64e5185f8
pacificyost <pacificyost@gmail.com>
2020-02-01 09:23:10 -0800

    Player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof (CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float rotateSpeed = 50f;
+
+    CharacterController characterController;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveByPlayerInput();
+    }
+
+    private void MoveByPlayerInput()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        characterController.Move(transform.forward * Time.deltaTime * moveSpeed * verticalInput);
+        transform.Rotate(0f, horizontalInput * Time.deltaTime * rotateSpeed, 0f);
+    }
+}
