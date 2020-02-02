a028ffba90e258e5c9d0c2a119740cf82501e1a0
David S <git@davidsharer.com>
2020-02-01 16:59:10 -0800

    Bars

---
+++ Assets/Materials/Bars/TrackPlayerOxygen.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackPlayerOxygen : MonoBehaviour
+{
+    public PlayerInteraction player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = new Vector3(transform.localScale.x, player.oxygen / 100f, transform.localScale.z);
+    }
+}
